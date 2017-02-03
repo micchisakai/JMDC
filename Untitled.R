2530858de30e910feb7af5668fc7ec0a1e8ef3bc
micchisakai <sakai.michi.7v@kyoto-u.ac.jp>
2017-02-03 14:34:28 +0900

    change

--- Untitled.R
+++ Untitled.R
@@ -1,1 +1,16 @@
-## test2
+git --version
+git config --global user.name 'shotera'
+git config --global user.email 'ohtera.s@gmail.com'
+git config --global --list
+
+git clone https://github.com/shotera/demo.git
+cd demo
+ls
+head README.md
+git remote show origin
+
+echo "This is a new line I wrote on my computer" >> README.md
+git status
+git add -A
+git commit -m "A commit from my local computer"
+git push
